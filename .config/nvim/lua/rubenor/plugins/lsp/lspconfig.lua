eda20288f9189a175ce3e9ee98e90651a0cc28e6
Nyx325 <omar.roman.2828@gmail.com>
2025-02-22 13:50:22 -0600

    lombook con jdtls funcional

--- .config/nvim/lua/rubenor/plugins/lsp/lspconfig.lua
+++ .config/nvim/lua/rubenor/plugins/lsp/lspconfig.lua
@@ -195,12 +195,47 @@ return {
 			end,
 
 			["jdtls"] = function()
-				-- configure jdtls (Java)
+				-- Obtener ruta de instalación de jdtls desde Mason
+				local mason_registry = require("mason-registry")
+				local jdtls_pkg = mason_registry.get_package("jdtls")
+				local jdtls_path = jdtls_pkg:get_install_path()
+
+				-- Ruta automática a lombok.jar
+				local lombok_path = jdtls_path .. "/lombok.jar"
+
+				-- Configurar cmd con argumentos necesarios
+				local cmd = {
+					"java",
+					"-Declipse.application=org.eclipse.jdt.ls.core.id1",
+					"-Dosgi.bundles.defaultStartLevel=4",
+					"-Declipse.product=org.eclipse.jdt.ls.core.product",
+					"-Dlog.protocol=true",
+					"-Dlog.level=ALL",
+					"-Xmx4G",
+					"--add-modules=ALL-SYSTEM",
+					"--add-opens",
+					"java.base/java.util=ALL-UNNAMED",
+					"--add-opens",
+					"java.base/java.lang=ALL-UNNAMED",
+					"-javaagent:" .. lombok_path, -- ¡Clave para Lombok!
+					"-jar",
+					vim.fn.glob(jdtls_path .. "/plugins/org.eclipse.equinox.launcher_*.jar"),
+					"-configuration",
+					jdtls_path .. "/config_linux", -- Ajustar para Win/Mac
+					"-data",
+					vim.fn.stdpath("data") .. "/jdtls_workspace",
+				}
+
 				lspconfig["jdtls"].setup({
+					cmd = cmd, -- Sobrescribe el comando por defecto
 					capabilities = capabilities,
 					settings = {
 						java = {
-							-- Configuración específica de Java
+							configuration = {
+								annotationProcessing = {
+									enabled = true, -- Procesamiento de anotaciones
+								},
+							},
 							completion = {
 								favoriteStaticMembers = {
 									"org.junit.Assert.*",
