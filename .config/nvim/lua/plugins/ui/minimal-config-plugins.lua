0f8013a6acd33b4d2678bed78e36f506be61bdee
Omar Román <omar.roman.2828@gmail.com>
2026-05-22 11:19:55 -0600

    iconos archivos blazor

--- .config/nvim/lua/plugins/ui/minimal-config-plugins.lua
+++ .config/nvim/lua/plugins/ui/minimal-config-plugins.lua
@@ -1,6 +1,15 @@
 vim.pack.add({
+	{ src = "https://github.com/nvim-mini/mini.icons" },
 	{ src = "https://github.com/rachartier/tiny-inline-diagnostic.nvim" },
 })
+
+require("mini.icons").setup({
+	extension = {
+		razor = { glyph = "", hl = "MiniIconsAzure" },
+	},
+})
+
+MiniIcons.mock_nvim_web_devicons()
 
 require("tiny-inline-diagnostic").setup({
 	preset = "modern", -- "modern", "classic", "minimal", "powerline"
--- .config/nvim/lua/plugins/utils/fzf-lua.lua
+++ .config/nvim/lua/plugins/utils/fzf-lua.lua
@@ -1,12 +1,7 @@
 -- Buscador en archivos, iconos, atajos lsp
 vim.pack.add({
-	{ src = "https://github.com/nvim-mini/mini.icons" },
 	{ src = "https://github.com/ibhagwan/fzf-lua" },
 })
-
-require("mini.icons").setup()
-
-MiniIcons.mock_nvim_web_devicons()
 
 local fzf = require("fzf-lua")
 
